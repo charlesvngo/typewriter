33f178ec5911c0c6c3ef649191f500ac1198217b
Charles Ngo <charlesngo37@gmail.com>
2022-04-12 09:56:00 -0700

    linted file

--- typewriter.js
+++ typewriter.js
@@ -5,7 +5,7 @@ sentence.split("").forEach((char, index) => {
   // print each character individually. Timer is based on the index of the string array.
   setTimeout(() => process.stdout.write(char), index * 50);
   if (index === sentence.length - 1) {
-    // if at the end of string, print a new line. 
+    // if at the end of string, print a new line.
     setTimeout(() => process.stdout.write('\n'), (index + 1) * 50);
   }
 });
