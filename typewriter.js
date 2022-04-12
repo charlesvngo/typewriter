b57109f33139e822b4af41052f6bc0b3b7c628e6
Charles Ngo <charlesngo37@gmail.com>
2022-04-12 09:48:41 -0700

    added async console logging

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+
+// split the string into characters
+sentence.split("").forEach((char, index) => {
+  // print each character individually. Timer is based on the index of the string array.
+  setTimeout(() => process.stdout.write(char), index * 50);
+  if (index === sentence.length - 1) {
+    // if at the end of string, print a new line. 
+    setTimeout(() => process.stdout.write('\n'), (index + 1) * 50);
+  }
+});
+
